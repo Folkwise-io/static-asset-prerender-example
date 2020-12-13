c64fcc779e83a72ad421521bdd51d75d0b0c704f
Monarch Wadia <monarch@mintbean.io>
2020-12-13 00:01:55 -0500

    Looks like we definitely need serve-static. We'll just modify it first...

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 const Express = require("express");
+import fs from "fs";
+import path from "path";
 
 const app = Express();
 
 app.get("/status", (req, res, next) => {
   res.json({
     isLive: true,
+  });
+});
+
+app.get("*", async (req, res, next) => {
+  const { originalUrl } = req;
+  await fs.readFile(path.join(__dirname, "..", "dist"));
+
+  res.json({
+    url: req.originalUrl,
   });
 });
 
